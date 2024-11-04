b04229d71d2c2a7122dee63187727e9178a53d71
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2024-11-04 22:53:11 +0100

    language

--- src/app/[lang]/dictionaries.js
+++ src/app/[lang]/dictionaries.js
@@ -1,12 +1,10 @@
+// app/[lang]/dictionaries.js
 const dictionaries = {
     en: () => import('./../dictionaries/en.json').then((module) => module.default),
     srb: () => import('./../dictionaries/srb.json').then((module) => module.default),
     hu: () => import('./../dictionaries/hu.json').then((module) => module.default),
+
   };
   
-  export const getDictionary = async (locale = 'hu') => { // Default to 'hu' if locale is undefined
-    if (!dictionaries[locale]) {
-      throw new Error(`Dictionary not found for locale: ${locale}`);
-    }
-    return dictionaries[locale]();
-  };
+  export const getDictionary = async (locale) => dictionaries[locale]();
+  

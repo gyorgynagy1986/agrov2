2d05846cddd0ca13a0c4ebd2252bc703c734aa53
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2024-11-04 22:51:07 +0100

    language

--- src/middleware.js
+++ src/middleware.js
@@ -2,28 +2,31 @@ import { NextResponse } from "next/server";
 import { match } from '@formatjs/intl-localematcher';
 import Negotiator from 'negotiator';
 
-const locales = ['en', 'srb', 'hu'];
-const defaultLocale = 'hu';
+const locales = ['en', 'srb', 'hu']; // Supported locales
+const defaultLocale = 'hu'; // Default language for root domain
 
 function getLocale(request) {
   const negotiator = new Negotiator({ headers: request.headers });
   const languages = negotiator.languages();
+
+  // Match the languages with the supported locales, or default to `defaultLocale`
   return match(languages, locales, defaultLocale) || defaultLocale;
 }
 
 export function middleware(request) {
   const { pathname } = request.nextUrl;
 
-  // Skip redirect if on the root path `/` (default to Hungarian)
+  // Redirect root path '/' to default locale if no prefix is provided
   if (pathname === '/') {
-    return NextResponse.next();
+    request.nextUrl.pathname = `/${defaultLocale}`;
+    return NextResponse.redirect(request.nextUrl);
   }
 
   const hasLocalePrefix = locales.some(
     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
   );
 
-  // Redirect if no locale prefix is in the pathname
+  // Redirect if no locale is in the pathname
   if (!hasLocalePrefix) {
     const locale = getLocale(request);
     request.nextUrl.pathname = `/${locale}${pathname}`;
